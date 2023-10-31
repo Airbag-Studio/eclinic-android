3c5744b0276a72c5b16f00de32c4f5a9f4f1ff7d
Stefano Rizzetto <stefano@airbagstudio.it>
2023-10-31 15:18:09 +0100

    fix user detiails grid button

--- app/src/main/java/it/airbagstudio/ticare/pages/patientDetails/GridButton.kt
+++ app/src/main/java/it/airbagstudio/ticare/pages/patientDetails/GridButton.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,9 +24,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontVariation.weight
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -62,61 +66,42 @@ fun GridButton(
             Modifier
         }
         val badgeOpacity = if (badgeCount > 0 && !isLoading) 1f else 0f
-        Spacer(modifier = Modifier.weight(0.2f))
-        Row(
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.Bottom,
-            modifier = Modifier
-                .alpha(badgeOpacity)
-                .padding(end = 16.dp)
-                .zIndex(1f)
-        ) {
-            Spacer(modifier = Modifier.weight(1f))
-            Text(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.error)
-                    .widthIn(min = 24.dp)
-                    .padding(horizontal = 5.dp),
-                text = "$badgeCount",
-                color = MaterialTheme.colorScheme.errorContainer,
-                textAlign = TextAlign.Center,
-                lineHeight = 25.sp
-            )
-        }
-
-
+        Spacer(modifier = Modifier.weight(0.25f))
         BoxWithConstraints {
             val boxWithConstraintsScope = this
             //You can use this scope to get the minWidth, maxWidth, minHeight, maxHeight in dp and constraints
-            val height = if(boxWithConstraintsScope.maxHeight > 120.dp ) 80.dp else boxWithConstraintsScope.maxHeight - 40.dp
+            val height = if(boxWithConstraintsScope.maxHeight > 120.dp ) 70.dp else boxWithConstraintsScope.maxHeight - 30.dp
             Column {
                 Image(
-                    modifier = Modifier.height(height = height),
+                    modifier = Modifier.fillMaxWidth().height(height = height),
                     painter = image,
+                    contentScale = ContentScale.Fit,
                     contentDescription = label,
                     alpha = if (isLoading) 0f else 1f
                 )
             }
+            Row(
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.Bottom,
+                modifier = Modifier
+                    .alpha(badgeOpacity)
+                    .padding(end = 16.dp)
+                    .zIndex(1f)
+            ) {
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.error)
+                        .widthIn(min = 24.dp)
+                        .padding(horizontal = 5.dp),
+                    text = "$badgeCount",
+                    color = MaterialTheme.colorScheme.errorContainer,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 25.sp
+                )
+            }
         }
-/*
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-
-                .heightIn(0.dp, 25.dp)
-                .weight(0.7f)
-                .then(boxModifier)
-        ) {
-            Image(
-                modifier = Modifier,
-                painter = image,
-                contentDescription = label,
-                alpha = if (isLoading) 0f else 1f
-            )
-        }
-
- */
         if (isLoading) {
             Box(
                 modifier = Modifier
@@ -131,11 +116,12 @@ fun GridButton(
 
             }
         } else {
-            Column(modifier = Modifier.weight(0.3f)) {
+            Column(modifier = Modifier.weight(0.5f).padding(top = 2.dp)) {
                 Text(
                     text = label,
                     textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.labelMedium,
+                    fontWeight = FontWeight(600),
                     color = MaterialTheme.colorScheme.primary,
                 )
                 Spacer(modifier = Modifier.weight(1f))
@@ -150,36 +136,71 @@ fun GridButton(
 @Preview
 private fun GridButtonPreview() {
     AppTheme() {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.height(280.dp)
-        ) {
-            GridButton(
-                modifier = Modifier.weight(1f),
-                image = painterResource(id = R.drawable.ic_wounds),
-                label = "ferite",
-                isLoading = true,
-                badgeCount = 23
+        Column(modifier = Modifier.background(Color.White)) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.height(280.dp)
             ) {
+                GridButton(
+                    modifier = Modifier.weight(1f),
+                    image = painterResource(id = R.drawable.ic_wounds),
+                    label = "ferite",
+                    isLoading = true,
+                    badgeCount = 23
+                ) {
 
+                }
+                GridButton(
+                    modifier = Modifier.weight(1f),
+                    image = painterResource(id = R.drawable.ic_wounds),
+                    label = "ferite",
+                    isLoading = false,
+                    badgeCount = 23
+                ) {
+
+                }
+                GridButton(
+                    modifier = Modifier.weight(1f),
+                    image = painterResource(id = R.drawable.ic_wounds),
+                    label = "ferite",
+                    isLoading = false,
+                    badgeCount = 2
+                ) {
+
+                }
             }
-            GridButton(
-                modifier = Modifier.weight(1f),
-                image = painterResource(id = R.drawable.ic_wounds),
-                label = "ferite",
-                isLoading = false,
-                badgeCount = 23
+            Divider()
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.height(80.dp)
             ) {
+                GridButton(
+                    modifier = Modifier.weight(1f),
+                    image = painterResource(id = R.drawable.ic_wounds),
+                    label = "ferite",
+                    isLoading = true,
+                    badgeCount = 23
+                ) {
 
-            }
-            GridButton(
-                modifier = Modifier.weight(1f),
-                image = painterResource(id = R.drawable.ic_wounds),
-                label = "ferite",
-                isLoading = false,
-                badgeCount = 2
-            ) {
+                }
+                GridButton(
+                    modifier = Modifier.weight(1f),
+                    image = painterResource(id = R.drawable.ic_wounds),
+                    label = "ferite",
+                    isLoading = false,
+                    badgeCount = 23
+                ) {
 
+                }
+                GridButton(
+                    modifier = Modifier.weight(1f),
+                    image = painterResource(id = R.drawable.ic_wounds),
+                    label = "ferite",
+                    isLoading = false,
+                    badgeCount = 2
+                ) {
+
+                }
             }
         }
     }
