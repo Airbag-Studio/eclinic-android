cfece53ad25d24146f82966b2efb6bac80081306
Giuseppe Silvestri <giuseppe@airbagstudio.it>
2023-12-06 16:30:46 +0100

    feat(consumption): create/edit screen viem model

--- app/src/main/java/it/airbagstudio/ticare/pages/consumptions/create/ConsumptionCreateViewModel.kt
+++ app/src/main/java/it/airbagstudio/ticare/pages/consumptions/create/ConsumptionCreateViewModel.kt
@@ -1,2 +1,150 @@
 package it.airbagstudio.ticare.pages.consumptions.create
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ch.ticare.eclinic.library.entity.SaveEmployeeConsumption
+import ch.ticare.eclinic.library.repository.ConsumptionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import it.airbagstudio.ticare.utils.format
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.util.Date
+import javax.inject.Inject
+
+data class ConsumptionCreateUiState(
+    val isSuccess: Boolean,
+    val title: String,
+    val errorMessage: String?,
+    val isLoading: Boolean,
+    val item: Item
+
+) {
+    data class Item(
+        val date: Date,
+        val quantity: Double,
+        val notes: String
+    )
+}
+
+@HiltViewModel
+class ConsumptionCreateViewModel @Inject constructor(
+    private val consumptionRepository: ConsumptionRepository
+) : ViewModel() {
+
+    private val isSuccess = MutableStateFlow<Boolean>(false)
+
+    private val articles = consumptionRepository.getArticles()
+    private val selectedArticleId = MutableStateFlow<Int?>(null)
+    private val consumptionId = MutableStateFlow<Int?>(null)
+    private val date = MutableStateFlow<Date>(Date())
+    private val quantity = MutableStateFlow<Double>(0.0)
+    private val notes = MutableStateFlow<String>("")
+
+    private val isLoading = MutableStateFlow<Boolean>(false)
+    private val errorMessage = MutableStateFlow<String?>(null)
+
+    private val selectedArticle = combine(articles, selectedArticleId){ _articles, _id ->
+        _articles.firstOrNull { it.id == _id }
+    }
+
+    private val consumption =
+        combine(date, quantity, notes) { date, quantity, notes ->
+            ConsumptionCreateUiState.Item(date, quantity, notes)
+        }
+
+    val uiState = combine(consumption, isLoading, errorMessage,selectedArticle,isSuccess) { consumption, isLoading, errorMessage,selectedArticle,isSuccess ->
+        ConsumptionCreateUiState(
+            title = selectedArticle?.desc ?: "",
+            errorMessage = errorMessage,
+            isLoading = isLoading,
+            item = consumption,
+            isSuccess = isSuccess
+        )
+    }.catch {
+        errorMessage.value = it.localizedMessage
+        isLoading.value = false
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        initialValue = ConsumptionCreateUiState(
+            title = "",
+            errorMessage = null,
+            isLoading = false,
+            item = ConsumptionCreateUiState.Item(Date(), 0.0, ""),
+            isSuccess = false
+        )
+    )
+
+    val coroutineExceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        throwable.printStackTrace()
+        errorMessage.value = throwable.localizedMessage
+        isLoading.value = false
+    }
+
+    fun setSelectedArticleId(id: Int?){
+        this.selectedArticleId.value = id
+    }
+
+    fun setConsumptionId(id: Int?){
+        this.consumptionId.value = id
+    }
+
+    fun setDate(date: Date){
+        this.date.value = date
+    }
+
+    fun setNotes(value:String){
+        this.notes.value = value
+    }
+
+    fun setQuantity(value: Double){
+        this.quantity.value = value
+    }
+
+    fun clearErrors() {
+        errorMessage.value = null
+    }
+
+    fun clearData() {
+        this.isSuccess.value = false
+        this.selectedArticleId.value = null
+        date.value = Date()
+        quantity.value = 0.0
+        notes.value = ""
+    }
+
+    fun saveConsumption() {
+        viewModelScope.launch(coroutineExceptionHandler) {
+            isLoading.value = true
+            val item = SaveEmployeeConsumption(
+                id = consumptionId.value,
+                idItem = selectedArticleId.value!!,
+                quantity = quantity.value,
+                remarks = notes.value,
+                date = date.value.format("yyyy.MM.dd")
+
+            )
+            if (item.id != null) {
+                val res = consumptionRepository.editConsumption(item)
+                res.error?.desc?.let {
+                    errorMessage.value = it
+                } ?: run{
+                    isSuccess.value = true
+                }
+            } else {
+                val res = consumptionRepository.addConsumption(item)
+                res.error?.desc?.let {
+                    errorMessage.value = it
+                } ?: run{
+                    isSuccess.value = true
+                }
+            }
+            isLoading.value = false
+        }
+    }
+}
