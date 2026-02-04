5c6bb90408c4a44beafbd204dbba4a814e894f81
StefanoRizzetto <stefanorizzetto83@gmail.com>
2026-02-04 10:34:44 +0100

    fix: MYC-304 Integrazione nel Diario di tutte le attività che vengono dai Piani di cura Domiciliari

--- app/src/main/java/it/airbagstudio/ticare/pages/diary/DiaryViewModel.kt
+++ app/src/main/java/it/airbagstudio/ticare/pages/diary/DiaryViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ch.ticare.eclinic.library.entity.DiaryItem
 import ch.ticare.eclinic.library.repository.DiaryRepository
+import ch.ticare.eclinic.library.repository.HomeCareActivitiesRepository
 import ch.ticare.eclinic.library.repository.UserDetailRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import it.airbagstudio.ticare.navigation.DestinationsArgs
+import it.airbagstudio.ticare.utils.SERVER_PARAMETER_DATE_TIME_FORMAT
 import it.airbagstudio.ticare.utils.format
 import it.airbagstudio.ticare.utils.getCompleteName
 import it.airbagstudio.ticare.utils.toDate
@@ -31,6 +33,7 @@ data class DiaryUIState(
 class DiaryViewModel @Inject constructor(
     private val userDetailRepository: UserDetailRepository,
     private val diaryRepository: DiaryRepository,
+    private val homeCareActivitiesRepository: HomeCareActivitiesRepository,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
@@ -38,8 +41,10 @@ class DiaryViewModel @Inject constructor(
     private val patientCod: String = savedStateHandle[DestinationsArgs.PATIENT_COD]!!
     private val isLoading = MutableStateFlow<Boolean>(false)
     private val items = MutableStateFlow<List<DiaryItem>>(listOf())
+    private val homeCareItems = MutableStateFlow<List<DiaryItem>>(listOf())
+
     val case = userDetailRepository.getCurrentCase()
-    val uiState = combine(items, isLoading) { items, _ ->
+    val uiState = combine(items, isLoading,homeCareItems) { items, _,homeCareItems ->
 
 
         DiaryUIState(
@@ -47,7 +52,7 @@ class DiaryViewModel @Inject constructor(
                 val date = it.date.toDate("dd.MM.yyyy")
                 date?.format("EEE dd MMMM") ?: it.date
             },
-            homeCareItems = items.filter { it.entityName == "HomeCareServiceTask" }.groupBy {
+            homeCareItems = homeCareItems.groupBy {
                 val date = it.date.toDate("dd.MM.yyyy")
                 date?.format("EEE dd MMMM") ?: it.date
             },
@@ -71,11 +76,35 @@ class DiaryViewModel @Inject constructor(
             val maxDiaryDays = diaryRepository.getMaxDiaryDays()
             val now = LocalDate.now()
             val from = now.minusDays(maxDiaryDays.toLong())
-            items.value = diaryRepository.getDiary(
-                patientCod,
-                from.format(dateFormatter),
-                now.format(dateFormatter)
-            ).results ?: listOf()
+            launch {
+                items.value = diaryRepository.getDiary(
+                    patientCod,
+                    from.format(dateFormatter),
+                    now.format(dateFormatter)
+                ).results ?: listOf()
+            }
+            launch {
+                homeCareItems.value = homeCareActivitiesRepository.getHomeCareActivities(
+                    code = patientCod,
+                    carePlanId = null,
+                    start = from.format(dateFormatter),
+                    end = now.format(dateFormatter)
+                ).results?.map {
+                    DiaryItem(
+                        caseCode = patientCod,
+                        date = it.execDateTime,
+                        entityName = "HomeCareServiceTask",
+                        time = it.execDateTime.toDate(SERVER_PARAMETER_DATE_TIME_FORMAT)
+                            ?.format("HH:mm") ?: "",
+                        patientLbl = "",
+                        title = "HomeCareServiceTask",
+                        typeLbl = it.type,
+                        userLbl = "",
+                        isScheduledTask = it.isScheduled,
+                        duration = it.duration
+                    )
+                } ?: listOf()
+            }
             isLoading.value = false
         }
     }
